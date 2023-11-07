38d307787f7ece08553570885836411f349ee76d
ricardoayala2510 <143136224+ricardoayala2510@users.noreply.github.com>
2023-11-07 16:56:05 -0700

    draft 2

---
+++ finalprojectRCK/FinalProject.Logic.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace FinalProject.Logic.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ finalprojectRCK/FinalProject.Logic/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace FinalProject.Logic;
+
+public class Class1
+{
+
+}
